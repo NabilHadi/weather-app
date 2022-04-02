1ba899cffabd618ec05aa96b2d0c429acf809e2a
Nabil Hadi <napilfadl@gmail.com>
2022-04-03 00:57:55 +0300

    added city search and temp  unit toggle

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-console.log("Hello Weather");
 const API_KEY = "36ce6e444ccdccbe388d184c5b4b92e8";
 
 async function fetchCityCoordinates(cityName) {
@@ -21,10 +20,125 @@ async function fetchWeatherData(coordinates) {
   return json;
 }
 
-fetchCityCoordinates("jeddah", 1)
+function getWeatherObject(weatherData) {
+  return {
+    temp: weatherData.main.temp,
+    feels_like: weatherData.main.feels_like,
+    cityName: weatherData.name,
+    weatherDescription: weatherData.weather[0].description,
+    iconId: weatherData.weather[0].icon,
+  };
+}
+
+const WeatherDisplayController = (function () {
+  const cityNameField = document.querySelector("#city-name");
+  const descriptionField = document.querySelector("#weather-description");
+  const tempField = document.querySelector("#temp");
+  const feelsLikeField = document.querySelector("#feels-like");
+  const weatherIcon = document.querySelector("#weather-icon");
+
+  const tempUnits = document.querySelectorAll(".temp-unit");
+
+  const cityInput = document.querySelector("#city-input");
+  const searchBtn = document.querySelector("#search-btn");
+  const toggleTempUnit = document.querySelector("#toggle-temp");
+
+  let weatherUnit = {
+    name: "celsius",
+    symbol: "°C",
+  };
+
+  const displayWeather = (weatherObj) => {
+    cityNameField.textContent = weatherObj.cityName;
+    descriptionField.textContent = weatherObj.weatherDescription;
+    if (weatherUnit.name === "fahrenheit") {
+      tempField.textContent = celsiusToFahrenheit(weatherObj.temp);
+      feelsLikeField.textContent = celsiusToFahrenheit(weatherObj.feels_like);
+    } else {
+      tempField.textContent = weatherObj.temp;
+      feelsLikeField.textContent = weatherObj.feels_like;
+    }
+    tempUnits.forEach((item) => {
+      item.textContent = weatherUnit.symbol;
+    });
+    weatherIcon.setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${weatherObj.iconId}@2x.png`
+    );
+  };
+
+  const celsiusToFahrenheit = (celsius) => {
+    return Math.round((Number(celsius) * (9 / 5) + 32) * 100) / 100;
+  };
+
+  const fahrenheitToCelsius = (fahrenheit) => {
+    return Math.round((Number(fahrenheit) - 32) * (5 / 9) * 100) / 100;
+  };
+
+  const changeToFahrenheit = () => {
+    if (weatherUnit.name === "fahrenheit") return;
+    weatherUnit = {
+      name: "fahrenheit",
+      symbol: "°F",
+    };
+    tempField.textContent = celsiusToFahrenheit(tempField.textContent);
+    feelsLikeField.textContent = celsiusToFahrenheit(
+      feelsLikeField.textContent
+    );
+    tempUnits.forEach((item) => {
+      item.textContent = weatherUnit.symbol;
+    });
+  };
+
+  const changeToCelsius = () => {
+    if (weatherUnit.name === "celsius") return;
+    weatherUnit = {
+      name: "celsius",
+      symbol: "°C",
+    };
+    tempField.textContent = fahrenheitToCelsius(tempField.textContent);
+    feelsLikeField.textContent = fahrenheitToCelsius(
+      feelsLikeField.textContent
+    );
+    tempUnits.forEach((item) => {
+      item.textContent = weatherUnit.symbol;
+    });
+  };
+
+  searchBtn.addEventListener("click", (e) => {
+    if (cityInput.value === "") {
+      alert("Please Enter city");
+      return;
+    }
+
+    fetchCityCoordinates(cityInput.value)
+      .then((cityCoord) => fetchWeatherData(cityCoord))
+      .then((weatherData) => {
+        displayWeather(getWeatherObject(weatherData));
+      });
+  });
+
+  toggleTempUnit.addEventListener("change", (e) => {
+    if (toggleTempUnit.checked) {
+      changeToFahrenheit();
+    } else {
+      changeToCelsius();
+    }
+  });
+
+  return {
+    displayWeather,
+    changeToFahrenheit,
+    changeToCelsius,
+  };
+})();
+
+fetchCityCoordinates("London", 1)
   .then((cityCoord) => {
     return fetchWeatherData(cityCoord);
   })
   .then((weatherData) => {
     console.log(weatherData);
+    const weatherObj = getWeatherObject(weatherData);
+    WeatherDisplayController.displayWeather(weatherObj);
   });
